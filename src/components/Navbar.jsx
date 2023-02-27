1cd1817d2892cd3d47ec7cd7c46d71169eba65af
Raphael-Soares <soaresphaell@gmail.com>
2023-02-27 17:48:38 -0300

    navbar refactored

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,83 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import "./Navbar.scss";
+import { MdOutlineSearch, MdOutlineNotificationsNone } from "react-icons/md";
+
+import styled from "styled-components";
+
+const scrollY = window.scrollY;
+
+const Container = styled.header`
+    background-color: ${scrollY > 0 ? "rgba(0, 0, 0, 0.8)" : "rgba(0, 0, 0, 0)"};
+
+    height: 10vh;
+    display: flex;
+    align-items: center;
+    padding: 0 2rem;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 100;
+`;
+const Menu = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+`;
+
+const Logo = styled.h1`
+    color: #fff;
+    font-size: 1.4rem;
+    font-weight: 700;
+`;
+
+const List = styled.ul`
+    display: flex;
+    list-style: none;
+    margin-left: 2rem;
+    align-items: center;
+`;
+const ListLeft = styled(List)`
+    margin-left: auto;
+    gap: 1rem;
+`;
+
+const ListItem = styled.li`
+    margin-left: 1rem;
+    color: #fff;
+`;
+
+const ListItemIcon = styled(ListItem)`
+    font-size: 2rem;
+`;
 
 function Navbar() {
     return (
-        <nav className="navbar">
-            <ul>
-                <li>
-                    <Link className="link" to="/">
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link className="link" to="/movies">
-                        Movies
-                    </Link>
-                </li>
-                <li>
-                    <Link className="link" to="/favorites">
-                        Favorites
-                    </Link>
-                </li>
-            </ul>
-        </nav>
+        <Container>
+            <Logo>
+                <Link to="/">MovieApp</Link>
+            </Logo>
+            <Menu>
+                <List>
+                    <ListItem>Inicio</ListItem>
+                    <ListItem>Series</ListItem>
+                    <ListItem>Filmes</ListItem>
+                    <ListItem>Bombando</ListItem>
+                    <ListItem>Minha Lista</ListItem>
+                    <ListItem>Navegar por idiomas</ListItem>
+                </List>
+                <ListLeft>
+                    <ListItemIcon>
+                        <MdOutlineSearch />
+                    </ListItemIcon>
+                    <ListItem>Infantil</ListItem>
+                    <ListItemIcon>
+                        <MdOutlineNotificationsNone />
+                    </ListItemIcon>
+                </ListLeft>
+            </Menu>
+        </Container>
     );
 }
 
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,12 +1,10 @@
-import {useParams} from "react-router-dom";
-import {useContext, useState, useEffect} from "react";
-import {MoviesContext} from "../contexts/MoviesData";
-
-import "./style/Movie.scss";
+import { useParams } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
+import { MoviesContext } from "../contexts/MoviesData";
 
 function Movie() {
-    const {id} = useParams();
-    const {movie, getMovie, favoriteMovie, movieImage, getMovieImage} = useContext(MoviesContext);
+    const { id } = useParams();
+    const { movie, getMovie, favoriteMovie, movieImage, getMovieImage } = useContext(MoviesContext);
 
     useEffect(() => {
         getMovie(id);
@@ -18,14 +16,20 @@ function Movie() {
             {movie && (
                 <div className="movie">
                     {movieImage && (
-                        <img src={`https://image.tmdb.org/t/p/original/${movieImage.posters[0].file_path}`} alt={movie.title} className="movie__image" />
+                        <img
+                            src={`https://image.tmdb.org/t/p/original/${movieImage.posters[0].file_path}`}
+                            alt={movie.title}
+                            className="movie__image"
+                        />
                     )}
                     <div className="movie__info">
                         <div className="movie__title">
                             {movie.title} <button onClick={() => favoriteMovie(movie)}>Fav</button>
                         </div>
                         <div className="movie__release">{movie.release_date.slice(0, 4)}</div>
-                        <div className="movie__genre">{movie.genres.map((genre) => genre.name + ", ")}</div>
+                        <div className="movie__genre">
+                            {movie.genres.map((genre) => genre.name + ", ")}
+                        </div>
                         <div className="movie__overview">{movie.overview}</div>
                     </div>
                 </div>
