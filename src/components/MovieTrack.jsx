dc7e0d07bddb0a646d8ecd47c3c18cd6eb44f647
Raphael-Soares <soaresphaell@gmail.com>
2023-03-03 16:21:12 -0300

    fix: move slider working

--- src/components/MovieTrack.jsx
+++ src/components/MovieTrack.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
-
+import axios from "axios";
 import ScrollMovieItem from "./ScrollMovieItem";
 
 import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
+
+const windowWidth = window.innerWidth;
 
 const Container = styled.div`
     height: 100%;
@@ -19,6 +21,7 @@ const Title = styled.h1`
     font-size: 1.1rem;
     font-weight: 700;
     letter-spacing: 0.5px;
+    padding-left: 2rem;
 `;
 
 const Track = styled.ul`
@@ -32,48 +35,70 @@ const Track = styled.ul`
     &::-webkit-scrollbar {
         display: none;
     }
+
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    scroll-behavior: smooth;
+    padding-left: 2rem;
 `;
 
 const Control = styled.button`
     position: absolute;
     top: 2.2rem;
+    background-color: transparent;
 
     height: calc(100% - 2.2rem);
     border: none;
-    font-size: 2rem;
+    font-size: 5rem;
+    color: transparent;
     cursor: pointer;
     transition: all 0.2s ease-in-out;
 
     &:hover {
+        color: #fff;
     }
 `;
 
-function MovieTrack({ movies, title }) {
+function MovieTrack({ genre: { title, url } }) {
+    const [movies, setMovies] = useState([]);
+    const trackRef = useRef(null);
+
+    const trackWidth = trackRef.current?.scrollWidth;
+
+    async function fetchMovies() {
+        const response = await axios.get(url);
+        setMovies(response.data.results);
+    }
+
+    function scrollLeft() {
+        trackRef.current.scrollLeft -= windowWidth;
+    }
+
+    function scrollRight() {
+        trackRef.current.scrollLeft += windowWidth;
+    }
+
+    useEffect(() => {
+        fetchMovies();
+    }, [url]);
+
     return (
         <Container>
             <Title>{title}</Title>
 
-            <Track>
-                {movies && movies.map((movie) => <ScrollMovieItem movie={movie} key={movie.id} />)}
-            </Track>
-
-            <Control
-                style={{
-                    left: 0,
-                    background: "linear-gradient(80deg, #000000 0%, rgba(0,0,0,0) 100%)",
-                }}
-            >
+            <Control style={{ left: 0 }} onClick={scrollLeft}>
                 <MdKeyboardArrowLeft />
             </Control>
 
-            <Control
-                style={{
-                    right: 0,
-                    background: "linear-gradient(270deg, #000000 0%, rgba(0,0,0,0) 100%)",
-                }}
-            >
+            <Control style={{ right: 0 }} onClick={scrollRight}>
                 <MdKeyboardArrowRight />
             </Control>
+
+            <Track ref={trackRef}>
+                {movies.map((movie, index) => (
+                    <ScrollMovieItem key={movie.id} movie={movie} />
+                ))}
+            </Track>
         </Container>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,63 +17,48 @@ const Body = styled.div`
     display: flex;
     flex-direction: column;
     margin-top: -4em;
-
-    padding: 0 2rem;
 `;
 
+const genres = [
+    {
+        title: "Trending Now",
+        url: `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    },
+
+    {
+        title: "Top Rated",
+        url: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    },
+    {
+        title: "Action Movies",
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    },
+    {
+        title: "Comedy Movies",
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    },
+    {
+        title: "Horror Movies",
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    },
+    {
+        title: "Romance Movies",
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    },
+    {
+        title: "Documentaries",
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    },
+];
+
 function Home() {
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const trending = await axios.get(
-                `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`
-            );
-            const topRated = await axios.get(
-                `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
-            );
-            const netflixOriginals = await axios.get(
-                `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`
-            );
-            const action = await axios.get(
-                `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`
-            );
-            const comedy = await axios.get(
-                `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`
-            );
-            const horror = await axios.get(
-                `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`
-            );
-            const romance = await axios.get(
-                `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`
-            );
-            const mystery = await axios.get(
-                `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=9648`
-            );
-
-            setData([
-                { title: "Popular on Netflix", movies: trending.data.results },
-                { title: "Top Rated", movies: topRated.data.results },
-                { title: "Netflix Originals", movies: netflixOriginals.data.results },
-                { title: "Action", movies: action.data.results },
-                { title: "Comedy", movies: comedy.data.results },
-                { title: "Horror", movies: horror.data.results },
-                { title: "Romance", movies: romance.data.results },
-                { title: "Mystery", movies: mystery.data.results },
-            ]);
-        };
-
-        fetchData();
-    }, []);
-
     return (
         <Container>
             <Banner />
             <Body>
-                {data &&
-                    data.map((genre) => (
-                        <MovieTrack key={genre.title} title={genre.title} movies={genre.movies} />
-                    ))}
+                {genres.map((genre) => (
+                    <MovieTrack key={genre.title} genre={genre} />
+                ))}
             </Body>
         </Container>
     );
