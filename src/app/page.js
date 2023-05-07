a881f7358fec48994a2382c7d94a9a6ba3047556
Raphael-Soares <soaresphaell@gmail.com>
2023-05-07 16:52:15 -0300

    boilerplate

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import Header from "@/components/Header";
+
 function page() {
-    return <div>page</div>;
+    return (
+        <div>
+            <Header></Header>
+            page
+        </div>
+    );
 }
 
 export default page;
